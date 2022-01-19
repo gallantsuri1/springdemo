0838d18677fa6d9c6aec1086c126d7a249626487
suri <gallantsuri1@gmail.com>
2022-01-18 23:14:47 -0600

    fixing mvn clean install

--- src/test/java/com/example/springdemo/SpringdemoApplicationTests.java
+++ src/test/java/com/example/springdemo/SpringdemoApplicationTests.java
@@ -2,6 +2,13 @@ package com.example.springdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class SpringdemoApplicationTests {
@@ -10,4 +17,17 @@ class SpringdemoApplicationTests {
 	void contextLoads() {
 	}
 
+}
+
+@Configuration
+class TestingDataSourceConfig {
+
+	@Bean
+	@Primary
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.generateUniqueName(true)
+				.setType(EmbeddedDatabaseType.H2)
+				.build();
+	}
 }
