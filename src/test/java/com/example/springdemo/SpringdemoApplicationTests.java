dc9d5b9177e171deacbfe0726c8e80ede7ab95c1
suri <gallantsuri1@gmail.com>
2022-02-19 02:08:48 -0600

    cleanup

--- src/test/java/com/example/springdemo/SpringdemoApplicationTests.java
+++ src/test/java/com/example/springdemo/SpringdemoApplicationTests.java
@@ -2,22 +2,13 @@ package com.example.springdemo;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import static org.mockito.Mockito.*;
-
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.Answer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
-import javax.sql.DataSource;
+import static org.mockito.Mockito.mockStatic;
 
-//@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class SpringdemoApplicationTests {
 
@@ -30,16 +21,3 @@ class SpringdemoApplicationTests {
 	}
 
 }
-
-//@Configuration
-//class TestingDataSourceConfig {
-//
-//	@Bean
-//	@Primary
-//	public DataSource dataSource() {
-//		return new EmbeddedDatabaseBuilder()
-//				.generateUniqueName(true)
-//				.setType(EmbeddedDatabaseType.H2)
-//				.build();
-//	}
-//}
